04ac61c4ba6c9caa1305e7715b232b8d3f326bb1
Iliav <iliav@hurdit.com>
2019-01-10 11:19:39 -0500

    Added start page layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import Routes from 'Routes';
+import Routes from './Routes';
 import { getUser } from 'actions/user';
 import './App.css';
 
@@ -12,7 +12,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div>
         <Routes />
       </div>
     );
